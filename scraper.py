bd73998f544a08a907a37322d810203c1c46a2fd
Uzumaki6969 <62650994+Uzumaki6969@users.noreply.github.com>
2020-03-28 20:56:39 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,9 +3,9 @@ from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
 import csv
  
-api_id = 123456
-api_hash = 'YOUR_API_HASH'
-phone = '+111111111111'
+api_id = 1232695
+api_hash = 84261ce3887863dbf27fcaad38ec0c01
+phone = +17199999062
 client = TelegramClient(phone, api_id, api_hash)
  
 client.connect()
